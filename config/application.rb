6d3ee09f70d867236d207c897ccc4cb11ab9d70b
Allan-Sartor <allansartor@outlook.com>
2022-05-28 18:43:29 -0300

    Created control for user, jwt token and validated

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module PontodeskServerV1
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.action_controller.default_protect_from_forgery = true
+    
     config.api_only = true
   end
 end
