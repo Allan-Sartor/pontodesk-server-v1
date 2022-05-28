ac49a060320d6ec18d7cbce1bdd7d7e65660f793
Allan-Sartor <allansartor@outlook.com>
2022-05-28 19:13:08 -0300

    Included validates for models

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,3 +1,9 @@
 class Call < ApplicationRecord
   belongs_to :user
+
+  validates :priority_level, presence: true
+  validates :anydesk_number, presence: true, length: { minimum: 9 }
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :call_status, presence: true
 end
