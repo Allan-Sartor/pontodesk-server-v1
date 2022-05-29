3ec189edcb398ee8845509270343f969ac4e66e7
Allan-Sartor <allansartor@outlook.com>
2022-05-29 19:41:27 -0300

    included user for response login

--- app/controllers/Api/v1/authentication_controller.rb
+++ app/controllers/Api/v1/authentication_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthenticationController < ApiController
     user = User.find_by(email: params[:email])
     puts user
     if user&.valid_password?(params[:password])
-      render json: { token: JsonWebToken.encode(sub: user.id) }
+      render json: { token: JsonWebToken.encode(sub: user.id), user: user }
     else
       render json: { errors: 'invalid' }
     end
