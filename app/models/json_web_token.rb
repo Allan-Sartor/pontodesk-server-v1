9c1e226d06ba68f815bb5c8d1a2300685c4ff3a3
Allan Sartor <allansartor@outlook.com>
2022-05-30 21:10:35 -0300

    Changes for JWT Token Generate

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,16 +1,12 @@
 class JsonWebToken
- # Key of Encryption our Descryption
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
-  # Encryption of user password
-  def self.encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+  def self.encode(payload)
+    expiration = 1.day.from_now.to_i
+    JWT.encode payload.merge(exp: expiration), SECRET_KEY 
   end
-
-  # Descryptuion of user password
+  
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]['user_id']
-    HashWithIndifferentAccess.new decoded
+    JWT.decode(token, SECRET_KEY).first
   end
 end
