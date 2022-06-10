1d365b699033808df5fc0790e129b04a11604640
Allan Sartor <allansartor@outlook.com>
2022-06-10 00:37:21 -0300

    Created Serializers for Users and Calls

--- app/serializers/call_serializer.rb
+++ app/serializers/call_serializer.rb
@@ -1,3 +1,11 @@
 class CallSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id,
+             :priority_level, 
+             :anydesk_number, 
+             :description, 
+             :image_url, 
+             :call_status,
+             :avg_score
+
+  has_many :review
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id,
+             :email,
+             :name,
+             :admin,
+             :created_at
+             
+  has_many :call
 end
