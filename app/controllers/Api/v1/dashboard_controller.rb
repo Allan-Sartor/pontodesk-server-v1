633155f83ed82b08618fc73578d6f54a2cdce2cf
Allan-Sartor <allansartor@outlook.com>
2022-06-30 17:52:03 -0300

    Refactor: Changes in response for json dash

--- app/controllers/Api/v1/dashboard_controller.rb
+++ app/controllers/Api/v1/dashboard_controller.rb
@@ -4,8 +4,12 @@ class Api::V1::DashboardController < ApiController
 
     @call_count_of_seven_days = Call.group_by_week(:created_at).count
 
-    @call_count_of_thirty_days = Call.group_by_week(:created_at).count
+    @call_count_of_thirty_days = Call.group_by_month(:created_at).count
 
-    render json: { call_count_of_seven_days: @call_count_of_seven_days, call_count_of_day: @call_count_of_day }, status: :ok
+    
+    render json: { call_count_of_day: @call_count_of_day,
+                   call_count_of_seven_days: @call_count_of_seven_days, 
+                   call_count_of_thirty_days: @call_count_of_thirty_days
+                  }, status: :ok
   end
 end
